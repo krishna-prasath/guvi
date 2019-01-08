b8acaf2feb17e8ff697b6ce6be3b4d92d7e021ee
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-01-08 16:17:35 +0530

    Create amstrongran.py

---
+++ amstrongran.py
@@ -0,0 +1,12 @@
+x,y=map(int,input().split())
+z=0
+
+for j in range(x,y):
+    z=j
+    a=0
+    for i in range(len(str(j))):
+        r=j%10
+        a=a+r**3
+        j=j//10
+    if a==z:
+       print(a,end=" ")
