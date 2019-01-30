c2a533cc0043ae97da258e3346d7c1107629ecf0
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-01-30 22:21:12 +0530

    Update zeroequal.py

--- zeroequal.py
+++ zeroequal.py
@@ -1,6 +1,8 @@
 a=int(input())
 v=list(map(int,input().split()))
-for i in range(len(v)):
-    for j in range(i+1,len(v)):
-        if v[i]+v[j]==0:
+for i in range(a):
+    for j in range(i+1,a):
+        if v[i]+v[j]==0 or v[i]+v[j]==1:
             print(v[i],v[j])
+            break
+# didint read the question properly
