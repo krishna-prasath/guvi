16f9f60b15daef84f3afd6961600d4aec40083f0
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-02-02 21:40:56 +0530

    Update nextletter.py

--- nextletter.py
+++ nextletter.py
@@ -1,8 +1,8 @@
 a=input()
 a1=""
 for i in a:
-    if ord(i)+1>90:
-        a1=a1+chr(((ord(i)+1)//26)+64)
+    if ord(i)+3>90:
+        a1=a1+chr(((ord(i)+3)//26)+64)
     else:
-        a1=a1+chr(ord(i)+1)
+        a1=a1+chr(ord(i)+3)
 print(a1)
