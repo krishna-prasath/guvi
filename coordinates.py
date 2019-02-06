fdf631dc8e588cfaa96fb24993d57d7d96fdb38f
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-02-06 21:15:46 +0530

    Update coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,8 +1,9 @@
 z=[]
 for i in range(3):
     z.append(list(map(int,input().split())))
-if z[0][0]==z[1][0]==z[2][0] or z[0][1]==z[1][1]==z[2][1]:
+if z[0][0]==z[1][0]==z[2][0] or z[0][1]==z[1][1]==z[2][1] or (z[0][0]==z[0][1] and z[1][0]==z[1][1] and z[2][0]==z[2][1]):
     print("yes")
 else:
     print("no")
     
+#missed one condition
