a38aef414e79205a7559366011291c34bed201ef
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-08 16:32:15 +0530

    Update vowelsort.py

--- vowelsort.py
+++ vowelsort.py
@@ -14,7 +14,7 @@ for i in range(len(l)):
     s.append(c)
 for i in range(len(s)):
     for j in range(i+1,len(s)):
-        if s[i]>s[j]:
+        if s[i]<s[j]:
             h=l[i]
             l[i]=l[j]
             l[j]=h
