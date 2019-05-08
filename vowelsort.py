ab4e718b0ed88e1c252944909e315a8b1c637e92
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-08 16:28:44 +0530

    Create vowelsort.py

---
+++ vowelsort.py
@@ -0,0 +1,23 @@
+a=int(input())
+l=[]
+for i in range(a):
+    l.append(input())
+
+s,d=[],[]
+a1=['a','e','i','o','u']
+for i in range(len(l)):
+    c=0
+    for j in range(len(l[i])):
+        for k in range(len(a1)):
+            if l[i][j].lower()==(a1[k]):
+                c=c+1
+    s.append(c)
+for i in range(len(s)):
+    for j in range(i+1,len(s)):
+        if s[i]>s[j]:
+            h=l[i]
+            l[i]=l[j]
+            l[j]=h
+for i in range(a):
+    print(l[i])
+
