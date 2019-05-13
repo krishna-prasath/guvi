6d6323d3947c04c01b74daf9c13c1ebc34272404
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-13 15:14:02 +0530

    Create single_num.py

---
+++ single_num.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+s=set(a)
+s=list(s)
+z,c=0,0
+for i in range(len(s)):
+    for j in range(len(a)):
+        if s[i]==a[j]:
+            c+=1
+    if c==1:
+        z=s[i]
+print(z)
