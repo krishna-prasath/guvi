b675444fd03a101dbaab6aad11589c94b0cb6363
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-18 10:55:26 +0530

    Update first_repeat.py

--- first_repeat.py
+++ first_repeat.py
@@ -1,9 +1,13 @@
 a=int(input())
 x=list(map(int,input().split()))
-z=[x[0]]
+z,c=[x[0]],0
 for i in range(1,a):
     if x[i] in z:
-        print(x[i])
+        c=x[i]        
         break
     else:
         z.append(x[i])
+if c!=0:
+    print(c)
+else:                               # slight changes for Unique
+    print("unique")
