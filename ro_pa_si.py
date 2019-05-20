13a1e678c89c07626161bd376e191eca9042b1b3
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-20 12:11:31 +0530

    Create ro_pa_si.py

---
+++ ro_pa_si.py
@@ -0,0 +1,7 @@
+a,b=map(str,input().split())
+if ((a=='R' and b=='P' )or(b=='R' and a=='P'))or((a=='R' and b=='S' )or(b=='S' and a=='R')):
+    print('R')
+elif(a=='S' and b=='P')or(b=='S' and a=='P'):
+    print('S')
+else:
+    print('D')
