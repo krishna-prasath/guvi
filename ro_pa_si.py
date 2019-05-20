7f8a204345df3d83117a8cbbb9526b381a804356
Krishna Prasath Bharath <42857124+krishna-prasath@users.noreply.github.com>
2019-05-20 12:14:36 +0530

    Update ro_pa_si.py

--- ro_pa_si.py
+++ ro_pa_si.py
@@ -1,7 +1,9 @@
 a,b=map(str,input().split())
-if ((a=='R' and b=='P' )or(b=='R' and a=='P'))or((a=='R' and b=='S' )or(b=='S' and a=='R')):
+if (a=='R' and b=='P' )or(b=='R' and a=='P'):
+    print('P')
+elif(a=='R' and b=='S' )or(b=='S' and a=='R'):
     print('R')
 elif(a=='S' and b=='P')or(b=='S' and a=='P'):
     print('S')
 else:
-    print('D')
+    print('D')          #when both player do's the same symbol
